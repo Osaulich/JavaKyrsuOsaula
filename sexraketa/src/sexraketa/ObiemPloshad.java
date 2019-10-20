3fd61938e70af2812a5fe25f63fae3dbee7966b9
Osaulich <dimaosaula@myrambler.ru>
2019-10-20 14:06:37 +0300

    1

---
+++ sexraketa/src/sexraketa/ObiemPloshad.java
@@ -0,0 +1,12 @@
+package sexraketa;
+
+public class ObiemPloshad {
+	public static void main(String[] args) {
+		int a = 7;
+		int V = a*a*a;
+		int S = 6*(a*a);
+			System.out.println(V);
+			System.out.println(S);
+	}
+
+}
