13218c7e06be41c9b884a3d594dfe4fb087c981a
Osaulich <dimaosaula@myrambler.ru>
2019-10-20 13:49:13 +0300

    1

---
+++ sexraketa/src/sexraketa/PerimetrPloshad.java
@@ -0,0 +1,13 @@
+package sexraketa;
+
+public class PerimetrPloshad {
+	public static void main(String[] args) {
+		int a = 11;
+		int b = 8;
+		int S = a*b;
+		int P = (a+b)*2;
+			System.out.println(S);
+			System.out.println(P);
+	}
+
+}
---
+++ sexraketa/src/sexraketa/ploshad.java
@@ -0,0 +1,11 @@
+package sexraketa;
+
+public class ploshad {
+	public static void main(String[] args) {
+		int a = 9;
+		int S = a*a;
+			System.out.println(S);
+		
+	}
+
+}
