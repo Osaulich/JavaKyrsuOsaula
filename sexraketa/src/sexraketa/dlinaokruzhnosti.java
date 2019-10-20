d13ac74eaeb373764eeaad1fb43b18dd27cf11d4
Osaulich <dimaosaula@myrambler.ru>
2019-10-20 14:02:24 +0300

    1

---
+++ sexraketa/src/sexraketa/dlinaokruzhnosti.java
@@ -0,0 +1,12 @@
+
+package sexraketa;
+
+public class dlinaokruzhnosti {
+	public static void main(String[] args) {
+		int d = 12;
+		double pi = 3.14;
+		double L = pi*d;
+			System.out.println(L);
+	}
+
+}
